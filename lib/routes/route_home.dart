fdc4af1d6e6ea5543da63f8c704d514bd66c5f21
Konstantin Rolf <konstantin.rolf@gmail.com>
2021-04-16 23:02:06 +0200

    Organize pubspec.yml

--- lib/routes/route_home.dart
+++ lib/routes/route_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RouteHome extends StatelessWidget {
-  const RouteHome({Key key}) : super(key: key);
+  const RouteHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
