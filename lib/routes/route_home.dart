b3d4980816320a9217808945f2861af291fade1f
Konstantin Rolf <konstantin.rolf@gmail.com>
2021-04-16 22:59:59 +0200

    Added basic setup

---
+++ lib/routes/route_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RouteHome extends StatelessWidget {
+  const RouteHome({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/routes/route_unknown.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RouteUnknown extends StatelessWidget {
+  const RouteUnknown({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
